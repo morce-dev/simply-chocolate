7b5febc9ed84e08e17ff17c17cbd27378ff3776c
morce-dev <morley@morce.dev>
2025-02-02 12:59:00 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import "./style.css";
+import "./common.css";
+import "./header.css";
+
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+const toggleMenu = () => {
+  navMenu.classList.toggle("is-open");
+};
+
+openMenu.addEventListener("click", toggleMenu);
+closeMenu.addEventListener("click", toggleMenu);
