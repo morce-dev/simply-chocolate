9d12ad10e133145a099ab4f7403d364e40b16f3c
morce-dev <morley@morce.dev>
2025-02-04 06:47:43 +0800

    Update

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -12,10 +13,24 @@ module.exports = {
       title: "Simply Chocolate",
       template: path.resolve(__dirname, "../src/index.html"),
       filename: "index.html",
+      link: [
+        {
+          rel: "preload",
+          href: "/fonts/my-font.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+      ],
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: "[name].css",
+    }),
+    new CompressionPlugin({
+      test: /\.(woff2|css|js)$/i,
+      algorithm: "brotliCompress",
+      filename: "[path][base].br",
     }),
   ],
   module: {
@@ -23,6 +38,13 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(woff2)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
       },
     ],
   },
