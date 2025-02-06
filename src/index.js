ba554cf8e11929561c5d9207ba0a1a736bcae505
morce-dev <morley@morce.dev>
2025-02-06 10:49:35 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import "./common.css";
 import "./header.css";
+import "./hero.css";
 
-const openMenu = document.querySelector(".open-menu");
-const closeMenu = document.querySelector(".close-menu");
-const navMenu = document.querySelector(".nav-menu");
+const bodyScrollLock = require("body-scroll-lock");
+const menuToggle = document.querySelector(".menu-toggle");
+const closeMenu = document.querySelector(".menu-close");
+const navMenu = document.querySelector(".nav-mobile");
+const navMenuList = document.querySelector(".nav-mobile .nav-menu-list");
 
 const toggleMenu = () => {
-  navMenu.classList.toggle("is-open");
+  const isActive = menuToggle.getAttribute("aria-expanded") === "true";
+  console.log("boolean value: ", isActive);
+  const string = "true";
+  console.log("non-boolean value: ", string);
+
+  menuToggle.setAttribute("aria-expanded", !isActive);
+
+  isActive
+    ? bodyScrollLock.enableBodyScroll(document.body)
+    : bodyScrollLock.disableBodyScroll(document.body);
+
+  navMenu.classList.toggle("active");
 };
 
-openMenu.addEventListener("click", toggleMenu);
+const scrollToSection = (event) => {
+  if (event.target.nodeName !== "A") return;
+  toggleMenu();
+};
+
+menuToggle.addEventListener("click", toggleMenu);
 closeMenu.addEventListener("click", toggleMenu);
+navMenuList.addEventListener("click", scrollToSection);
